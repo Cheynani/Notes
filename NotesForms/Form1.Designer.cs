bd30cd494c545ab2f4f069bf849cd9fe10a39b19
Cheynani <104343938+Cheynani@users.noreply.github.com>
2022-04-25 09:38:11 +0300

    Initial commit

---
+++ NotesForms/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace NotesForms
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if(disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.button3 = new System.Windows.Forms.Button();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.tabControl1.SuspendLayout();
+			this.tabPage1.SuspendLayout();
+			this.tabPage2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// tabControl1
+			// 
+			this.tabControl1.Controls.Add(this.tabPage1);
+			this.tabControl1.Controls.Add(this.tabPage2);
+			this.tabControl1.Location = new System.Drawing.Point(1, 1);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(636, 293);
+			this.tabControl1.TabIndex = 0;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.BackColor = System.Drawing.Color.WhiteSmoke;
+			this.tabPage1.Controls.Add(this.button2);
+			this.tabPage1.Controls.Add(this.button1);
+			this.tabPage1.Controls.Add(this.label1);
+			this.tabPage1.Controls.Add(this.textBox1);
+			this.tabPage1.Controls.Add(this.listBox1);
+			this.tabPage1.Location = new System.Drawing.Point(4, 24);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(628, 265);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "Заметки";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(561, 6);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(26, 23);
+			this.button2.TabIndex = 4;
+			this.button2.Text = "✖";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Visible = false;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(593, 6);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(26, 23);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "✔";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Visible = false;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoEllipsis = true;
+			this.label1.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+			this.label1.Location = new System.Drawing.Point(195, 6);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(360, 25);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Название заметки";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(195, 34);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBox1.Size = new System.Drawing.Size(424, 229);
+			this.textBox1.TabIndex = 1;
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 15;
+			this.listBox1.Location = new System.Drawing.Point(0, 4);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.ScrollAlwaysVisible = true;
+			this.listBox1.Size = new System.Drawing.Size(189, 259);
+			this.listBox1.TabIndex = 0;
+			this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.BackColor = System.Drawing.Color.WhiteSmoke;
+			this.tabPage2.Controls.Add(this.button3);
+			this.tabPage2.Controls.Add(this.textBox3);
+			this.tabPage2.Controls.Add(this.textBox2);
+			this.tabPage2.Location = new System.Drawing.Point(4, 24);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(628, 265);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "Добавить заметку";
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(491, 239);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(130, 23);
+			this.button3.TabIndex = 2;
+			this.button3.Text = "Добавить заметку";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(6, 39);
+			this.textBox3.Multiline = true;
+			this.textBox3.Name = "textBox3";
+			this.textBox3.PlaceholderText = "Текст заметки";
+			this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBox3.Size = new System.Drawing.Size(615, 194);
+			this.textBox3.TabIndex = 1;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(7, 8);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.PlaceholderText = "Название заметки";
+			this.textBox2.Size = new System.Drawing.Size(198, 23);
+			this.textBox2.TabIndex = 0;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(638, 296);
+			this.Controls.Add(this.tabControl1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.Text = "Заметки";
+			this.tabControl1.ResumeLayout(false);
+			this.tabPage1.ResumeLayout(false);
+			this.tabPage1.PerformLayout();
+			this.tabPage2.ResumeLayout(false);
+			this.tabPage2.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private TabControl tabControl1;
+		private TabPage tabPage1;
+		private ListBox listBox1;
+		private TabPage tabPage2;
+		private Button button1;
+		private Label label1;
+		private TextBox textBox1;
+		private Button button2;
+		private Button button3;
+		private TextBox textBox3;
+		private TextBox textBox2;
+	}
+}
---
+++ NotesForms/Form1.cs
@@ -0,0 +1,197 @@
+using System.Data.SQLite;
+using System.Text.RegularExpressions;
+
+namespace NotesForms
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			SQLiteDataReader sql_read;
+			SQLiteConnection con = new SQLiteConnection("Data Source=C:\\Users\\One\\Desktop\\IT Hub\\NotesForms\\NotesForms\\bin\\Debug\\net6.0-windows\\Notes.db");
+			con.Open();
+
+			listBox1.Items.Clear();
+			SQLiteCommand sql = con.CreateCommand();
+			sql.CommandText = "SELECT Title FROM Notes";
+
+			sql.ExecuteNonQuery();
+			sql_read = sql.ExecuteReader();
+			while(sql_read.Read())
+			{
+				listBox1.Items.Add(sql_read.GetString(0));
+			}
+			sql_read.Close();
+			con.Close();
+			listBox1.SetSelected(0, true);
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			SQLiteDataReader sql_read;
+			SQLiteConnection con = new SQLiteConnection("Data Source=C:\\Users\\One\\Desktop\\IT Hub\\NotesForms\\NotesForms\\bin\\Debug\\net6.0-windows\\Notes.db");
+			con.Open();
+			SQLiteCommand sql = con.CreateCommand();
+
+			string title = label1.Text;
+			string str = textBox1.Text;
+			sql.CommandText = "SELECT Title, Text FROM Notes";
+			sql.ExecuteNonQuery();
+			sql_read = sql.ExecuteReader();
+			while(sql_read.Read())
+			{
+				if(title.Equals(sql_read.GetString(0)))
+				{
+					if(str != sql_read.GetString(1))
+					{
+						string message = "Изменить эту заметку?";
+						string caption = "Изменение заметки";
+						var result = MessageBox.Show(message, caption, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+						if(result == DialogResult.Yes)
+						{
+							Regex rgx = new Regex(@"'");
+							if(!rgx.IsMatch(str))
+							{
+								SQLiteCommand sql1 = con.CreateCommand();
+								sql1.CommandText = $"UPDATE Notes set Text = '{str}' where Title = '{title}'";
+								sql1.ExecuteNonQuery();
+							}
+							else
+							{
+								message = "Символ «'» является недопустимым, замените его другим сиволом или уберите его из текста.";
+								caption = "Встречен недопустимый символ";
+								result = MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+							}
+						}
+					}
+				}
+			}
+			sql_read.Close();
+			con.Close();
+		}
+
+		private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if(listBox1.SelectedItem != null)
+			{
+				SQLiteDataReader sql_read;
+				string curItem = listBox1.SelectedItem.ToString();
+				SQLiteConnection con = new SQLiteConnection("Data Source=C:\\Users\\One\\Desktop\\IT Hub\\NotesForms\\NotesForms\\bin\\Debug\\net6.0-windows\\Notes.db");
+				con.Open();
+
+				SQLiteCommand sql = con.CreateCommand();
+				sql.CommandText = "SELECT Title, Text FROM Notes";
+
+				sql.ExecuteNonQuery();
+				sql_read = sql.ExecuteReader();
+				while(sql_read.Read())
+				{
+					if(curItem.Equals(sql_read.GetString(0)))
+					{
+						textBox1.Text = sql_read.GetString(1);
+						label1.Text = sql_read.GetString(0);
+						button1.Visible = true;
+						button2.Visible = true;
+					}
+
+				}
+				sql_read.Close();
+				con.Close();
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			const string message = "Вы хотите удалить эту заметку?";
+			const string caption = "Удаление заметки";
+			var result = MessageBox.Show(message, caption, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+			if(result == DialogResult.Yes)
+			{
+				SQLiteDataReader sql_read;
+				SQLiteConnection con = new SQLiteConnection("Data Source=C:\\Users\\One\\Desktop\\IT Hub\\NotesForms\\NotesForms\\bin\\Debug\\net6.0-windows\\Notes.db");
+				con.Open();
+				SQLiteCommand sql = con.CreateCommand();
+
+				string title = label1.Text;
+				sql.CommandText = "SELECT Title FROM Notes";
+				sql.ExecuteNonQuery();
+				sql_read = sql.ExecuteReader();
+				while(sql_read.Read())
+				{
+					if(title.Equals(sql_read.GetString(0)))
+					{
+						SQLiteCommand sql1 = con.CreateCommand();
+						sql1.CommandText = $"DELETE from Notes where Title = '{title}'";
+						sql1.ExecuteNonQuery();
+						listBox1.Items.Remove(title);
+						textBox1.Text = "";
+						label1.Text = "Название заметки";
+						button1.Visible = false;
+						button2.Visible = false;
+					}
+				}
+				sql_read.Close();
+				con.Close();
+			}
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			SQLiteDataReader sql_read;
+			SQLiteConnection con = new SQLiteConnection("Data Source=C:\\Users\\One\\Desktop\\IT Hub\\NotesForms\\NotesForms\\bin\\Debug\\net6.0-windows\\Notes.db");
+			con.Open();
+			SQLiteCommand sql = con.CreateCommand();
+			string title = textBox2.Text;
+			string text = textBox3.Text;
+			if(title != "")
+			{
+				int err = 0;
+				sql.CommandText = "SELECT Title FROM Notes";
+				sql.ExecuteNonQuery();
+				sql_read = sql.ExecuteReader();
+				while(sql_read.Read())
+				{
+					if(title == sql_read.GetString(0))
+					{
+						err = 1;
+					}
+				}
+				sql_read.Close();
+				if(err != 1)
+				{
+					Regex rgx = new Regex(@"'");
+					if(!rgx.IsMatch(text) && !rgx.IsMatch(title))
+					{
+						sql.CommandText = $"INSERT INTO Notes values(NULL, '{title}', '{text}')";
+						sql.ExecuteNonQuery();
+						con.Close();
+						listBox1.Items.Add(title);
+						textBox2.Text = "";
+						textBox3.Text = "";
+					}
+					else
+					{
+						const string message = "Символ «'» является недопустимым, замените его другим сиволом или уберите его.";
+						const string caption = "Встречен недопустимый символ";
+						var result = MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+					}
+				}
+				else
+				{
+					const string message = "Такая заметка уже существует, измените её название.";
+					const string caption = "Измените название заметки";
+					var result = MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+				}
+			}
+			else
+			{
+				const string message = "Заполните поле «Название заметки»!";
+				const string caption = "Ошибка";
+				var result = MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+	}
+}
